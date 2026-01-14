45e94393beb9e20bacdb33e163caa3d3eefe7b58
Gustavo Gomes <88335206+gustavogomespl@users.noreply.github.com>
2026-01-13 23:48:33 -0300

    Reapply "feat: add train2/test2 search patterns for whale competition"
    This reverts commit b90612c564c69830819063e618df1c330e964468.

--- kaggle_agents/utils/data_audit.py
+++ kaggle_agents/utils/data_audit.py
@@ -302,6 +302,8 @@ def audit_audio_competition(
         "raw_audio",
         "train_audio",
         "train",
+        "train2",  # for whale competition (non-standard naming)
+        "test2",   # for whale competition (non-standard naming)
         "essential_data",
     ]
 
